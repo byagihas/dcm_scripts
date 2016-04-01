f1bed765e4f305295928092dbce561a1e78469ea
Brandon Yagihashi <brayagih@publicisgroupe.net>
2016-04-01 12:42:48 -0500

    Updated build_campaign_list.py

--- build_campaign_list.py
+++ build_campaign_list.py
@@ -1,19 +1,23 @@
+# Brandon Yagihashi - 4/1/2016
+# Campaign List Builder
+# This script takes a profile ID and Advertiser ID and writes all relevant campaign names and dates to an HTML file - "campaign_list.html"
+# Also prints how many days until start date if it has not already launched. Need to convert to business days.
+
 import sys
 import csv
 import argparse
 import datetime
 from time import strptime
+from datetime import datetime, timedelta
+import pandas as pd
+from pandas.tseries.offsets import BDay #Possible business day workaround - Python Data Analysis Lib
 
 import dfareporting_utils
 from oauth2client import client
 
 argparser = argparse.ArgumentParser(add_help=False)
-argparser.add_argument(
-    'profile_id', type=int,
-    help='The ID of the profile')
-argparser.add_argument(
-    'advertiser_id', type=int,
-    help='The ID of the Advertiser')
+argparser.add_argument('profile_id', type=int, help='The ID of the profile')
+argparser.add_argument('advertiser_id', type=int,help='The ID of the Advertiser')
 
 def main(argv):
     flags = dfareporting_utils.get_arguments(argv, __doc__, parents=[argparser])
@@ -23,17 +27,26 @@ def main(argv):
 
     profile_id = flags.profile_id
     advertiser_id = flags.advertiser_id
-    current_date=datetime.datetime.now()
+
+    #Pending business day implementation
+    #today = pd.datetime.today()
+    #BDexample = today - BDay(4) 
     
     try:
-        htmlfile = open('Campaign_list.html', "w")
+        #Open HTML file for writing
+        htmlfile = open('campaign_list.html', "w")
         htmlfile.write('<table>')
         request = service.campaigns().list(profileId=profile_id, advertiserIds=advertiser_id).execute()
         for campaigns in request['campaigns']:
             startDate = strptime(campaigns['startDate'], "%Y-%m-%d")
-            htmlfile.write(("<tr>" + "<td>" + campaigns['name'] + "</td>" + "<td>" + str(campaigns['lastModifiedInfo']) + "</td>" + "<td>" + str(startDate[0]) +  str(startDate[1]) +  str(startDate[2]) + "</td>" + "</tr>").encode('utf-8'))
-        htmlfile.write('</table>')
+            htmlfile.write(("<tr>" + "<td>" + campaigns['name'] + "</td>" + "<td>" + str(startDate[1]) +  "-" + str(startDate[2]) + "-" + str(startDate[0]) + "</td>").encode('utf-8'))
+            if(datetime(startDate[0],startDate[1],startDate[2]) > datetime.now()):
+                htmlfile.write(("<td>" + str(datetime(startDate[0],startDate[1],startDate[2]) - datetime.now()) + "</td>" + "</tr>"))
+            else:
+                htmlfile.write("</tr>")                                
             
+        htmlfile.write('</table>')
+        
     except client.AccessTokenRefreshError:
         print ('The credentials have been revoked or expired, please re-run the '
            'application to re-authorize')
